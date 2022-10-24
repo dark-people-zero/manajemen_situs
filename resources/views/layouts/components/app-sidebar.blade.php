38ac6f1baea2c534c1b8b2ec4996c3ad5b6e82d2
Dark People <dark.people.zero@gmail.com>
2022-10-25 06:53:30 +0700

    config auto full di env

--- resources/views/layouts/components/app-sidebar.blade.php
+++ resources/views/layouts/components/app-sidebar.blade.php
@@ -57,17 +57,19 @@
                         </li>
                     @endif
 
-                    <li class="slide">
-                        <a href="/gitpull" class="side-menu__item">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="side-menu__icon">
-                                <rect x="2" y="2" width="20" height="8" rx="2" ry="2"/>
-                                <rect x="2" y="14" width="20" height="8" rx="2" ry="2"/>
-                                <line x1="6" y1="6" x2="6.01" y2="6"/>
-                                <line x1="6" y1="18" x2="6.01" y2="18"/>
-                            </svg>
-                            <span class="side-menu__label">Update Server</span>
-                        </a>
-                    </li>
+                    @if (env('AUTO_PULL_ACTIVE'))
+                        <li class="slide">
+                            <a href="/gitpull" class="side-menu__item">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="side-menu__icon">
+                                    <rect x="2" y="2" width="20" height="8" rx="2" ry="2"/>
+                                    <rect x="2" y="14" width="20" height="8" rx="2" ry="2"/>
+                                    <line x1="6" y1="6" x2="6.01" y2="6"/>
+                                    <line x1="6" y1="18" x2="6.01" y2="18"/>
+                                </svg>
+                                <span class="side-menu__label">Update Server</span>
+                            </a>
+                        </li>
+                    @endif
                 @endif
 
             </ul>
